8988c216fb14aad72bdf6b1170a5fc36645dc70d
Marcioguima <marciosouzaguima@gmail.com>
2022-02-08 23:58:52 -0300

    exercicio variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,26 @@
+//deve aparecer 10 e 10 e 5
+// 10, 10 e 10
+//let cargaHoraria 
+//let valorHorasTrabalhadas
+
+let meuNome
+let minhaIdade
+console.log(typeof meuNome, typeof minhaIdade)
+//deu indefinido porque eu não defini os valores
+
+let nome = prompt("qual seu nome?")
+let idade = prompt("qual sua idade?")
+console.log(typeof nome, typeof idade)
+
+//apareceu string string
+
+console.log("olá meu nome", nome, "minha idade é", idade)
+
+ const sim = true
+ const não = false
+
+ let pergunta = sim
+
+ 
+
+
