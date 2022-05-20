d839880784db11bc74a873075a464c11f0ba4a31
unknown <marciosouzaguima@gmail.com>
2022-05-19 22:20:41 -0300

    labenusers

---
+++ modulo4/apis-labenusers/labenusers/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import axios, { Axios } from 'axios';
+
+export default function App() {
+
+  const criarUsuario = () => {
+    
+    
+    
+  }
+
+
+  
+  
+
+return (
+<div className= "App">
+<input placeholder="Nome"/>
+<input placeholder="Email"/>
+<button onClick={criarUsuario}>Criar Usuário</button>
+</div>
+);
+}
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                     
