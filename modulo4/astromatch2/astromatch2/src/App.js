91513943e43d10dd1fa0516ff10acdb5c489af6e
unknown <marciosouzaguima@gmail.com>
2022-06-17 17:08:33 -0300

    novo-astromatch

---
+++ modulo4/astromatch2/astromatch2/src/App.js
@@ -0,0 +1,12 @@
+import { Matches } from "./Matches";
+import { Profile } from "./Profile";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Profile />
+      <Matches />
+    </div>
+  );
+}
