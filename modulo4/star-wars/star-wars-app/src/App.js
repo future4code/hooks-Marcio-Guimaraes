3f8ec625fc701924b26d544168f6c14cc45517de
unknown <marciosouzaguima@gmail.com>
2022-07-19 21:27:09 -0300

    projeto-atualizado

--- modulo4/star-wars/star-wars-app/src/App.js
+++ modulo4/star-wars/star-wars-app/src/App.js
@@ -1,8 +1,12 @@
+import CharacterListPage from "./CharacterListPage/CharacterListPage";
 
 
 export default function App () {
 return (
+ <div>
   <h1>Star Wars</h1>
+  <CharacterListPage/>
+  </div>
 );
 
 }
