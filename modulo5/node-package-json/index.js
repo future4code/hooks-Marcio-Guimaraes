1f808c2d52484b49cafe15cefd9a81fe5d04f571
unknown <marciosouzaguima@gmail.com>
2022-08-09 19:06:53 -0300

    node-package

---
+++ modulo5/node-package-json/index.js
@@ -0,0 +1,14 @@
+//b
+const nome = process.argv[2]
+const idade = Number(process.argv[3])
+
+
+console.log(`Olá ${nome}! Você tem ${idade} anos.`)
+
+//c
+
+const nome2 = process.argv[2]
+const idade2 = Number(process.argv[3])
+const novaIdade = idade2 + 7
+
+console.log(`Olá ${nome}! Você tem ${idade} anos. Em sete anos você terá ${novaIdade} anos`)
